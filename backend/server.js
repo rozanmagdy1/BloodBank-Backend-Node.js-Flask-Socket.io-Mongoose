80ee8eabe98427cd5d52ed1ab4376060a3f414ce
rozanmagdy1 <rozanmagdy1@gmail.com>
2023-06-25 15:44:10 +0300

    alpha

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 const { AdminMiddleWare } = require("./Middlewares/adminMiddleWare");
 const { WebsiteMiddleWare } = require("./Middlewares/websiteMiddleWare");
-
 const { adminRoutes } = require("./Routes/AdminRoutes/adminRoutes");
 const { WebsiteRoutes } = require("./Routes/WebsiteRoutes/websiteRoutes");
-const mongoose = require("mongoose");
 
 const app = express();
 const adminApp = express();
